304d58e6bb0c8d8eba5d457090a0f0c2bba7ca1f
Hill <hill@omnibpm.com>
2020-04-30 14:25:00 +0800

    fixed josn parse

--- support/record-id.sh
+++ support/record-id.sh
@@ -6,4 +6,4 @@ RECORD_NAME=`${DIR}/record-name.sh`
 RECORD_ID=`DEBUG=off ${DIR}/cloudflare-curl.sh -m GET -p dns_records?name=$RECORD_NAME` 
 
 
-echo $RECORD_ID | sed -n 's|.*"id":"\([^"]*\)".*|\1|p'
+echo $RECORD_ID | sed -n 's|.*"id"\s*:\s*"\([^"]*\)".*|\1|p'
